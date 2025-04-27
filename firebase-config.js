196f50348f36d894b9823c41c4e9a2d5a462408b
JumpedMocha3 <ziad5203@gmail.com>
2025-04-27 18:07:39 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA5h26hiZRX2WA3tRCjUY2NlA6rnRV0E24",
   authDomain: "namozag-3b281.firebaseapp.com",
@@ -15,8 +15,10 @@ const firebaseConfig = {
   measurementId: "G-CE1RNHKMQG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Get Firebase Auth and Firestore instances
 const auth = getAuth(app);
 const db = getFirestore(app);
 
