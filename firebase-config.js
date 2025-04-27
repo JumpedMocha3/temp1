af67d100c9a8b6af3f5534448dc22ca38e79b46d
JumpedMocha3 <ziad5203@gmail.com>
2025-04-27 17:23:04 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 const firebaseConfig = {
   apiKey: "AIzaSyA5h26hiZRX2WA3tRCjUY2NlA6rnRV0E24",
   authDomain: "namozag-3b281.firebaseapp.com",
+  databaseURL: "https://namozag-3b281-default-rtdb.firebaseio.com",
   projectId: "namozag-3b281",
   storageBucket: "namozag-3b281.firebasestorage.app",
   messagingSenderId: "183249580094",
-  appId: "1:183249580094:web:af1070013478332698616f"
+  appId: "1:183249580094:web:af1070013478332698616f",
+  measurementId: "G-CE1RNHKMQG"
 };
 
 const app = initializeApp(firebaseConfig);
